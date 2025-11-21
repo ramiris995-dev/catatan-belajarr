a8246dcd86bad3d7756067c2c581c9b91065ea8b
ramiristhegreat <ramiris995@gmail.com>
2025-11-22 05:27:23 +0700

    Update tipe_data.py

--- 01_dasar/tipe_data.py
+++ 01_dasar/tipe_data.py
@@ -1,1 +1,10 @@
+nama = "Budi"         # string
+umur = 20             # int
+tinggi = 170.5        # float
+is_student = True     # bool
+
+print(type(nama))
+print(type(umur))
+print(type(tinggi))
+print(type(is_student))
 
